248bac4eadae4d67ad16e3afc62213ff32895d9f
Abdul-Aleem Mohammed <abaleemmohammed@gmail.com>
2025-06-13 15:21:21 -0500

    added all of the buttons

---
+++ script.js
@@ -0,0 +1,26 @@
+// variables
+
+// number buttons
+    zeroButton = document.querySelector(".zero-button");
+    oneButton = document.querySelector(".one-button");
+    twoButton = document.querySelector(".two-button");
+    threeButton = document.querySelector(".three-button");
+    fourButton = document.querySelector(".four-button");
+    fiveButton = document.querySelector(".five-button");
+    sixButton = document.querySelector(".six-button");
+    sevenButton = document.querySelector(".seven-button");
+    eightButton = document.querySelector(".eight-button");
+    nineButton = document.querySelector(".nine-button");
+
+// operation buttions 
+    divisionButton = document.querySelector(".division-button");
+    multiplicationButton = document.querySelector(".multiplication-button");
+    additionButton = document.querySelector(".addition-button");
+    subtractionButton = document.querySelector(".subtraction-button");
+    clearButton = document.querySelector(".clear-button");
+    enterButton = document.querySelector(".enter-button")
+
+// functions 
+
+
+// buttons 
